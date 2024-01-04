15c3bd729e2de8507c492d11ef2b0fff5ea55d76
ttat288 <tranthamanhtoan288@gmail.com>
2024-01-04 15:49:39 +0700

    update UI

--- src/components/Layout/DefaultLayout/Sidebar/index.js
+++ src/components/Layout/DefaultLayout/Sidebar/index.js
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import {
-  Box,
   Drawer,
   DrawerBody,
   DrawerCloseButton,
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 //Layout
-import { HeaderOnly } from "../components/Layout";
+// import { HeaderOnly } from "../components/Layout";
 
 import Home from "../pages/Home";
 import Following from "../pages/Following";
